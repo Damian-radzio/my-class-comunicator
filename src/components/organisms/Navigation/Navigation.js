5d49eac946f2e55ab3d3a5259acfcdc1a2165569
Damian-Radzio <drado2003@gmail.com>
2021-05-08 14:42:50 +0200

    Add absolute imports

---
+++ src/components/organisms/Navigation/Navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from 'components/atoms/button/Button';
+export const Navigation = () => {
+  return (
+    <div>
+      <Button>123</Button>
+    </div>
+  );
+};
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,12 +1,10 @@
-import { GlobalStyles } from '../assets/styles/GlobalStyles';
-import { theme } from '../assets/styles/theme';
+import { GlobalStyles } from 'assets/styles/GlobalStyles';
+import { theme } from 'assets/styles/theme';
 import { ThemeProvider } from 'styled-components';
-import { Button } from '../components/atoms/button/Button';
 const Root = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Button>click here</Button>
     </ThemeProvider>
   );
 };
