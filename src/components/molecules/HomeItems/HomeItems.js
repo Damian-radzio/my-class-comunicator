8b867cdbc3430f23af70533552bb27d0b43b636e
Damian-radzio <drado2003@gmail.com>
2021-06-10 00:30:55 +0200

    add lessons hours to home section

--- src/components/molecules/HomeItems/HomeItems.js
+++ src/components/molecules/HomeItems/HomeItems.js
@@ -1,24 +1,45 @@
 import { StyledInfoSection, Wrapper } from './HomeItems.style';
+import { LessonsPlan } from 'data/LessonPlan/LessonPlan';
 import React from 'react';
+const date = new Date().getDay();
 
 export const HomeItems = () => {
   return (
     <Wrapper>
       <StyledInfoSection>
-        <p>
-          you start your lessons at: <span>8:45</span>
-        </p>
+        {LessonsPlan.map(({ day, start }) => (
+          <p>
+            {day === date ? (
+              <p>
+                {' '}
+                you start your lessons at:<span>{start}</span>
+              </p>
+            ) : null}
+          </p>
+        ))}
       </StyledInfoSection>
       <StyledInfoSection>
-        <p>
-          you have <span>7</span> lessons today
-        </p>
+        {LessonsPlan.map(({ day, amount }) => (
+          <p>
+            {day === date ? (
+              <p>
+                you have <span>{amount}</span> lessons today
+              </p>
+            ) : null}
+          </p>
+        ))}
       </StyledInfoSection>
 
       <StyledInfoSection>
-        <p>
-          You finish your lessons at:<span>15:50</span>
-        </p>
+        {LessonsPlan.map(({ day, end }) => (
+          <p>
+            {day === date ? (
+              <p>
+                you finish your lessons at:<span>{end}</span>
+              </p>
+            ) : null}
+          </p>
+        ))}
       </StyledInfoSection>
     </Wrapper>
   );
---
+++ src/data/LessonPlan/LessonPlan.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export const LessonsPlan = [
+  {
+    day: 1,
+    start: '8:00',
+    amount: '8',
+    end: '15:05',
+  },
+  {
+    day: 2,
+    start: '8:00',
+    amount: '7',
+    end: '14:05',
+  },
+  {
+    day: 3,
+    start: '10:45',
+    amount: '7',
+    end: '16:40',
+  },
+  {
+    day: 4,
+    start: '8:55',
+    amount: '7',
+    end: '15:05',
+  },
+  {
+    day: 5,
+    start: '8:00',
+    amount: '6',
+    end: '13:20',
+  },
+];
