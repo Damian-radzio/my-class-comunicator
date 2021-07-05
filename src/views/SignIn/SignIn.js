63548c79c9f3173cd6f208763f05128fec60542e
Damian-radzio <drado2003@gmail.com>
2021-07-06 00:11:49 +0200

    changed login api

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -6,7 +6,11 @@ import { LoginButton } from './SignIn.style';
 export const SignIn = () => {
   const [logged, setLogged] = useState(false);
   const login = () => {
-    setLogged(true);
+    fetch(
+      'https://graph.facebook.com/v11.0/me?fields=id%2Cname&access_token=EAAFqnKe7B6ABAA4iO2GcCStqxYkskzITyIFUZCBEUSM5dazBV9PgLzfKAN6JTCqZAOMlhqgBRZCniJXVcEgzX4x7r8KZA2NINGeVMVjdpiwKzZAf61lkdHknxOg4MTS01yvS7tjS6emfZC8nhm9Pv8vfwfwg9JoEfcRFNc4JI02FGeYPJeORncZAlDVEEj9JqcAMLsZARkybaff1LPtqu8AY'
+    )
+      .then((response) => response.json)
+      .then((data) => console.log(data));
   };
   return (
     <>
