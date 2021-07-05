4a243b7694613acff1b05d257a34d207f2974043
Damian-radzio <drado2003@gmail.com>
2021-07-06 00:15:57 +0200

    change fetch api url

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -7,7 +7,7 @@ export const SignIn = () => {
   const [logged, setLogged] = useState(false);
   const login = () => {
     fetch(
-      'https://graph.facebook.com/v11.0/me?fields=id%2Cname&access_token=EAAFqnKe7B6ABAA4iO2GcCStqxYkskzITyIFUZCBEUSM5dazBV9PgLzfKAN6JTCqZAOMlhqgBRZCniJXVcEgzX4x7r8KZA2NINGeVMVjdpiwKzZAf61lkdHknxOg4MTS01yvS7tjS6emfZC8nhm9Pv8vfwfwg9JoEfcRFNc4JI02FGeYPJeORncZAlDVEEj9JqcAMLsZARkybaff1LPtqu8AY'
+      'https://graph.facebook.com/v11.0/me?fields=id%2Cname&access_token=EAAFqnKe7B6ABALoLicMJi5ANo9TMZANLzjvuTgviIYAJY5T3onzh2GrLakhkdZAW12geBjpaDciIWtuIG08opc7ZAORokzxe41qp1PLGhzu6vtC9sZB1EVKnPDAkqHhGrTa5LZAXc9rtXt2LSaWsVCEEtiWeWnRC5LdRIGj4ZBKfA4CBKp1gRI9FS8qxZCMxPJhh6iJEAolrc00uCey5ZByZBzEckT5wQpAvaTY7jSxEFGhyUmYka5Bcl'
     )
       .then((response) => response.json())
       .then((data) => console.log(data));
