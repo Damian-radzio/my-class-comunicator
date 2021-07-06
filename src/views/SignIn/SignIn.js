2f48291a00a4474bbfc7cb83363c1b335b80dcd5
Damian-radzio <drado2003@gmail.com>
2021-07-06 18:58:40 +0200

    changed login api

--- src/views/SignIn/SignIn.js
+++ src/views/SignIn/SignIn.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import { Navigation } from 'components/organisms/Navigation/Navigation';
 import { Dashboard } from 'views/Dashboard/Dashboard';
 import { LoginButton } from './SignIn.style';
@@ -7,7 +6,7 @@ export const SignIn = () => {
   const [logged, setLogged] = useState(false);
   const login = () => {
     fetch(
-      'https://graph.facebook.com/v11.0/me?fields=id%2Cname&access_token=EAAFqnKe7B6ABALoLicMJi5ANo9TMZANLzjvuTgviIYAJY5T3onzh2GrLakhkdZAW12geBjpaDciIWtuIG08opc7ZAORokzxe41qp1PLGhzu6vtC9sZB1EVKnPDAkqHhGrTa5LZAXc9rtXt2LSaWsVCEEtiWeWnRC5LdRIGj4ZBKfA4CBKp1gRI9FS8qxZCMxPJhh6iJEAolrc00uCey5ZByZBzEckT5wQpAvaTY7jSxEFGhyUmYka5Bcl'
+      'https://graph.facebook.com/v11.0/me?fields=id%2Cname&access_token=EAAFqnKe7B6ABAEEJWigzvMySNfXhZAXjEEZCFleFXfVzM4te2ZATZB5NRwYbhR8IaQZA2Fo4PhFKZBk0ZCoYqwGvquTT6ZA3ZCEIJzWjVvjygr2ZADUXsigLJDYJDsZBmpR603uYGh7haQfjMdjFBQuu38OZAsq7UVUd4IzD5KZBNjZC3FD9q4HojLrI9Famw0mdWVFuqsyxffid1djuiJFkypHAgM5pq4lTZBZAZAzAZBVQR9EmOZCZAIqWPmwsceS7'
     )
       .then((response) => response.json())
       .then((data) => console.log(data));
